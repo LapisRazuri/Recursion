b3687de95a47aec06b6b28796adff80cd8b6bd92
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2024-07-25 16:33:49 +0200

    CreateFibonacchiRecursion

---
+++ index.js
@@ -0,0 +1,31 @@
+function fibs(num) {
+    
+    const arr = [0, 1];
+    let sum = 0;
+    let first = arr[0];
+    let second = arr[1];
+
+    for(let i = 0; i < num - 2; i++){
+        sum = first + second;
+        arr.push(sum);
+
+        first = second;
+        second = sum;
+    }
+ return arr;
+}
+
+console.log(fibs(10))
+
+function fibsRec(n, arr = [0, 1] ) {
+    
+    console.log("This was printed recursively");
+    if (arr.length >= n ){
+        return arr;
+    }
+
+    let nextFib = arr[arr.length - 1] + arr[arr.length - 2];
+    arr.push(nextFib)
+
+    return fibsRec(n, arr);
+}
